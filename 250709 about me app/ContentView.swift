7360bb3999b2ecbaed8462ad9ebb964fe04ba446
Ivy Sham <ivysham888@gmail.com>
2025-07-09 23:38:51 +0800

    Initial Commit

---
+++ 250709 about me app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  250709 about me app
+//
+//  Created by Ivy Sham on 9/7/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 250709 about me app/_50709_about_me_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _50709_about_me_appApp.swift
+//  250709 about me app
+//
+//  Created by Ivy Sham on 9/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct _50709_about_me_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
