6b38746f143084bcae2ffa7d20741c97d102ce8c
Ivy Sham <ivysham888@gmail.com>
2025-07-10 00:12:45 +0800

    added about me content: self introduction; added images (cartoon ivy, coding)

--- 250709 about me app/ContentView.swift
+++ 250709 about me app/ContentView.swift
@@ -22,9 +22,41 @@ struct ContentView: View {
                     .cornerRadius(25)
                 
                 VStack {
-                        
+                    ScrollView {
+                        VStack {
+                            Text("ABOUT ME")
+                                .font(.system(size: 50, weight: .bold))
+                                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+                                .multilineTextAlignment(.center)
+                                .underline()
+                            
+                            Text("Ivy S")
+                                .font(.custom("AmericanTypewriter", fixedSize: 37))
+                                .padding(EdgeInsets(top: 5, leading: 0, bottom: 0, trailing: 0))
+                            
+                            HStack(alignment: .top) {
+                                Image("ivycartoon")
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text("Hi! My name is Ivy and I am interested in coding! I hope to continue to improve my skills :)")
+                                        .alignmentGuide(.leading) { $0[.leading] }
+                                        
+                                }
+                                
+                            }
+                            
+                            
+                            
+                            Spacer()
+                        }
                     }
-                    .padding()
+                    
+                }
+                
+                    .frame(width: 375, height: 750)
+                    
         
             }
     }
