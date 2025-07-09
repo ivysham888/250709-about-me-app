85128d425ecad48d8071e2375a8db94c02968ddb
Ivy Sham <ivysham888@gmail.com>
2025-07-09 23:46:04 +0800

    created outer background (yellow) and inner/content background (rounded white)

--- 250709 about me app/ContentView.swift
+++ 250709 about me app/ContentView.swift
@@ -9,13 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+            ZStack {
+                
+                Rectangle()
+                    .fill(Color.yellow)
+                    .ignoresSafeArea()
+                
+                Rectangle()
+                    .fill(Color.white)
+                    .frame(width: 375, height: 750)
+                    .cornerRadius(25)
+                
+                VStack {
+                        
+                    }
+                    .padding()
+        
+            }
     }
 }
 
